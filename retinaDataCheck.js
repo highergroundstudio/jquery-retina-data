0b2b23f375275027f28e82bfcf532056e0eb41c9
Kyle King <kyle.king@highergroundstudio.com>
2013-04-23 11:45:20 -0600

    Create retinaDataCheck.js

---
+++ retinaDataCheck.js
@@ -0,0 +1,30 @@
+/*
+    Name: Retina data
+    Description: A jquery retina display via data plugin
+    Author: Kyle King (http://highergroundstudio.com)
+    Date: 04/23/2013
+    Version: 1.0.0
+*/
+
+(function( $ ){
+    $.fn.retinaData = function() {
+        // if(window.devicePixelRatio >= 2) {
+        if(true){
+            // i = index
+            // e = element
+            // r = retina url
+            // h = holder
+            this.each(function(i, e, r, h) {
+                e=$(e);
+                if(!e.attr('src')||!e.data('retina'))return;
+                r = e.data('retina');
+                h = e.attr('src', r).removeClass('retina').addClass('retina-displayed');
+                // check if image exists before displaying
+                $.ajax({url: r, type: "HEAD", success: function() {
+                  h;
+                }});
+            });
+        }
+        return this;
+    }
+})( jQuery );
