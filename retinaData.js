b58b1eedf90ddcdc06de571efd931ca4a51b9888
Kyle King <kyle.king@highergroundstudio.com>
2013-04-23 11:45:41 -0600

    Update retinaData.js
    fix...oops

--- retinaData.js
+++ retinaData.js
@@ -18,7 +18,7 @@
                 e=$(e);
                 if(!e.attr('src')||!e.data('retina'))return;
                 r = e.data('retina');
-                h = e.attr('src', r).removeClass('retina').addClass('retina-displayed');
+                e.attr('src', r).removeClass('retina').addClass('retina-displayed');
             });
         }
         return this;
